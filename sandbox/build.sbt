55ede9b4b259abf2e3ae7139707baa9381fc6a80
christoph <chkeller@gmx.net>
2016-08-19 10:25:38 +0200

    Minimal inital setup of sbt project for sandbox

---
+++ sandbox/build.sbt
@@ -0,0 +1,5 @@
+name := "sandbox"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
---
+++ sandbox/src/test/scala/sandbox/HelloWorldTest.scala
@@ -0,0 +1,11 @@
+package sandbox
+
+import org.scalatest.FunSuite
+
+class HelloWorldTest extends FunSuite {
+
+  test("hello world test") {
+    assert(true === true)
+  }
+
+}
