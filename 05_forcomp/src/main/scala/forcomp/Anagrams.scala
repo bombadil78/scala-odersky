c210eddd1bf20fe482dcf1523e16ff6dfb98f47d
christoph <chkeller@gmx.net>
2016-08-31 16:51:02 +0200

    Partial implementation of anagram exercise

--- 05_forcomp/src/main/scala/forcomp/Anagrams.scala
+++ 05_forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -8,86 +8,59 @@ object Anagrams {
   type Occurrences = List[(Char, Int)]
 
   val dictionary: List[Word] = loadDictionary
-  def wordOccurrences(w: Word): Occurrences = ???
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
-  def wordAnagrams(word: Word): List[Word] = ???
 
-  /** Returns the list of all subsets of the occurrence list.
-   *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
-   *  is a subset of `List(('k', 1), ('o', 1))`.
-   *  It also include the empty subset `List()`.
-   *
-   *  Example: the subsets of the occurrence list `List(('a', 2), ('b', 2))` are:
-   *
-   *    List(
-   *      List(),
-   *      List(('a', 1)),
-   *      List(('a', 2)),
-   *      List(('b', 1)),
-   *      List(('a', 1), ('b', 1)),
-   *      List(('a', 2), ('b', 1)),
-   *      List(('b', 2)),
-   *      List(('a', 1), ('b', 2)),
-   *      List(('a', 2), ('b', 2))
-   *    )
-   *
-   *  Note that the order of the occurrence list subsets does not matter -- the subsets
-   *  in the example above could have been displayed in some other order.
-   */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def wordOccurrences(w: Word): Occurrences = (w.groupBy[Char](c => c.toLower) map (x => x._1 -> x._2.length) toList).sorted
 
-  /** Subtracts occurrence list `y` from occurrence list `x`.
-   *
-   *  The precondition is that the occurrence list `y` is a subset of
-   *  the occurrence list `x` -- any character appearing in `y` must
-   *  appear in `x`, and its frequency in `y` must be smaller or equal
-   *  than its frequency in `x`.
-   *
-   *  Note: the resulting value is an occurrence - meaning it is sorted
-   *  and has no zero-entries.
-   */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = s flatMap (word => wordOccurrences(word))
 
-  /** Returns a list of all anagram sentences of the given sentence.
-   *
-   *  An anagram of a sentence is formed by taking the occurrences of all the characters of
-   *  all the words in the sentence, and producing all possible combinations of words with those characters,
-   *  such that the words have to be from the dictionary.
-   *
-   *  The number of words in the sentence and its anagrams does not have to correspond.
-   *  For example, the sentence `List("I", "love", "you")` is an anagram of the sentence `List("You", "olive")`.
-   *
-   *  Also, two sentences with the same words but in a different order are considered two different anagrams.
-   *  For example, sentences `List("You", "olive")` and `List("olive", "you")` are different anagrams of
-   *  `List("I", "love", "you")`.
-   *
-   *  Here is a full example of a sentence `List("Yes", "man")` and its anagrams for our dictionary:
-   *
-   *    List(
-   *      List(en, as, my),
-   *      List(en, my, as),
-   *      List(man, yes),
-   *      List(men, say),
-   *      List(as, en, my),
-   *      List(as, my, en),
-   *      List(sane, my),
-   *      List(Sean, my),
-   *      List(my, en, as),
-   *      List(my, as, en),
-   *      List(my, sane),
-   *      List(my, Sean),
-   *      List(say, men),
-   *      List(yes, man)
-   *    )
-   *
-   *  The different sentences do not have to be output in the order shown above - any order is fine as long as
-   *  all the anagrams are there. Every returned word has to exist in the dictionary.
-   *
-   *  Note: in case that the words of the sentence are in the dictionary, then the sentence is the anagram of itself,
-   *  so it has to be returned in this list.
-   *
-   *  Note: There is only one anagram of an empty sentence.
-   */
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary groupBy[Occurrences] (word => wordOccurrences(word))
+
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
+
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+
+    def split(el: (Char, Int)): Occurrences = {
+      val (myChar, myCount) = el
+      if (myCount == 0)
+        List()
+      else
+        el :: split((myChar, myCount - 1))
+    }
+
+    if (occurrences.isEmpty) {
+      List(List())
+    } else {
+      val oldCombinations = combinations(occurrences.tail)
+      val newElements = split(occurrences.head)
+      val newCombinations = for {
+        newElement <- newElements
+        oldCombination <- oldCombinations
+      }
+        yield newElement :: oldCombination
+      newCombinations ::: oldCombinations
+    }
+  }
+
+  // todo: improve using foldLeft (=> see examples)
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+
+    val subtractedFromX = for {
+      xElem <- x.toMap
+      yElem <- y.toMap
+      if (xElem._1 == yElem._1)
+    }
+      yield (xElem._1, xElem._2 - yElem._2)
+
+    val subtractedFromXWithoutZeroOrBelow = subtractedFromX.filter(x => x._2 > 0)
+
+    val onlyInX = for {
+      xElem <- x.toMap
+      if (!y.toMap.contains(xElem._1))
+    }
+      yield (xElem)
+
+    (onlyInX ++ subtractedFromXWithoutZeroOrBelow).toList
+  }
+
   def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
 }
--- 05_forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ 05_forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -37,8 +37,6 @@ class AnagramsSuite extends FunSuite  {
     assert(wordAnagrams("player").toSet === Set("parley", "pearly", "player", "replay"))
   }
 
-
-
   test("subtract: lard - r") {
     val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
     val r = List(('r', 1))
@@ -66,7 +64,6 @@ class AnagramsSuite extends FunSuite  {
     )
     assert(combinations(abba).toSet === abbacomb.toSet)
   }
-
 
   test("sentence anagrams: []") {
     val sentence = List()
@@ -99,5 +96,4 @@ class AnagramsSuite extends FunSuite  {
     )
     assert(sentenceAnagrams(sentence).toSet === anas.toSet)
   }
-
 }
